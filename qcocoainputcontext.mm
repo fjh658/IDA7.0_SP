7d92b4a0900029d60df2d39a120ac998c6bb27ed
fjh658 <fjh658@gmail.com>
2018-08-13 15:16:00 +0800

    add modified source code, qcocoainputcontext.mm

---
+++ qcocoainputcontext.mm
@@ -0,0 +1,150 @@
+/****************************************************************************
+**
+** Copyright (C) 2015 The Qt Company Ltd.
+** Contact: http://www.qt.io/licensing/
+**
+** This file is part of the plugins of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL21$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see http://www.qt.io/terms-conditions. For further
+** information use the contact form at http://www.qt.io/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 or version 3 as published by the Free
+** Software Foundation and appearing in the file LICENSE.LGPLv21 and
+** LICENSE.LGPLv3 included in the packaging of this file. Please review the
+** following information to ensure the GNU Lesser General Public License
+** requirements will be met: https://www.gnu.org/licenses/lgpl.html and
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** As a special exception, The Qt Company gives you certain additional
+** rights. These rights are described in The Qt Company LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "qnsview.h"
+#include "qcocoainputcontext.h"
+#include "qcocoanativeinterface.h"
+#include "qcocoawindow.h"
+
+#include <Carbon/Carbon.h>
+#import <Foundation/Foundation.h>
+
+#include <QtCore/QRect>
+#include <QtGui/QGuiApplication>
+#include <QtGui/QWindow>
+
+QT_BEGIN_NAMESPACE
+
+/*!
+    \class QCocoaInputContext
+    \brief Cocoa Input context implementation
+
+    Handles input of foreign characters (particularly East Asian)
+    languages.
+
+    \section1 Testing
+
+    \list
+    \o Select input sources like 'Kotoeri' in Language & Text Preferences
+    \o Compile the \a mainwindows/mdi example and open a text window.
+    \o In the language bar, switch to 'Hiragana'.
+    \o In a text editor control, type the syllable \a 'la'.
+       Underlined characters show up, indicating that there is completion
+       available. Press the Space key two times. A completion popup occurs
+       which shows the options.
+    \endlist
+
+    \section1 Interaction
+
+    Input method support in Cocoa uses NSTextInput protorol. Therefore
+    almost all functionality is implemented in QNSView.
+
+    \ingroup qt-lighthouse-cocoa
+*/
+
+
+
+QCocoaInputContext::QCocoaInputContext()
+    : QPlatformInputContext()
+    , mWindow(QGuiApplication::focusWindow())
+{
+    QMetaObject::invokeMethod(this, "connectSignals", Qt::QueuedConnection);
+    updateLocale();
+}
+
+QCocoaInputContext::~QCocoaInputContext()
+{
+}
+
+/*!
+    \brief Cancels a composition.
+*/
+
+void QCocoaInputContext::reset()
+{
+    QPlatformInputContext::reset();
+
+    if (!mWindow)
+        return;
+
+    QNSView *view = static_cast<QCocoaWindow *>(mWindow->handle())->qtView();
+    if (!view)
+        return;
+
+    QMacAutoReleasePool pool;
+    if (NSTextInputContext *ctxt = [NSTextInputContext currentInputContext]) {
+        [ctxt discardMarkedText];
+        [view unmarkText];
+    }
+}
+
+void QCocoaInputContext::connectSignals()
+{
+    connect(qApp, SIGNAL(focusObjectChanged(QObject*)), this, SLOT(focusObjectChanged(QObject*)));
+    focusObjectChanged(qApp->focusObject());
+}
+
+void QCocoaInputContext::focusObjectChanged(QObject *focusObject)
+{
+    Q_UNUSED(focusObject);
+    mWindow = QGuiApplication::focusWindow();
+}
+
+void QCocoaInputContext::updateLocale()
+{
+    /* https://bugreports.qt.io/browse/QTBUG-48772
+
+    TISInputSourceRef source = TISCopyCurrentKeyboardInputSource();
+    CFArrayRef languages = (CFArrayRef) TISGetInputSourceProperty(source, kTISPropertyInputSourceLanguages);
+    if (CFArrayGetCount(languages) > 0) {
+        CFStringRef langRef = (CFStringRef)CFArrayGetValueAtIndex(languages, 0);
+        QString name = QCFString::toQString(langRef);
+        QLocale locale(name);
+        if (m_locale != locale) {
+            m_locale = locale;
+            emitLocaleChanged();
+        }
+        CFRelease(langRef);
+    }
+    
+    */
+
+    QString name = QString::fromNSString([[NSLocale currentLocale] objectForKey:NSLocaleIdentifier]);
+    QLocale locale(name);
+    if (m_locale != locale) {
+        m_locale = locale;
+        emitLocaleChanged();
+    }
+}
+
+QT_END_NAMESPACE
